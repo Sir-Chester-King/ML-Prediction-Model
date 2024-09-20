1e60d932e3672c87351b95722e7de7b0b32071af
Sir-Chester-King <177522077+Sir-Chester-King@users.noreply.github.com>
2024-09-20 11:43:26 +0200

    Scratch blueprint

--- src/main.py
+++ src/main.py
@@ -4,36 +4,7 @@ import os
 import dotenv
 import pandas
 
-# ---- MAIN CODE SECTION ---- #
-
-"""Machine learning model to predict the potential purchase of a car of a person, using 
-the dataset as source input.
-
-Import and processing DataSet.
-"""
-
-
-def main():
-    dotenv.load_dotenv()  # Load the .env file to gather the path of CSV file.
-    dataset = pandas.read_csv(os.getenv('path_data'))  # Gather the dataset from the CSV file.
-
-    # Gather all the values in the columns from the dataset, except the last one; as a numpy array.
-    # Used the ILOC indexer in PANDAS to select the subset of the data and store that in the variable.
-    matrix_features_independent = dataset.iloc[:, :-1].values
-
-    # Gather all the values in the last column from the dataset; as a numpy array.
-    # Used the ILOC indexer in PANDAS to select the subset of the data and store that in the variable.
-    vector_features_dependent = dataset.iloc[:, -1].values
-
-    print("Matrix Features Independent:", end='\n')
-    print(matrix_features_independent)
-
-    print("Vector Features Dependent:", end='\n')
-    print(vector_features_dependent)
-
-
-# ---- MAIN CODE SECTION ---- #
-
+# ---- IMPORT SECTION ---- #
 
 # ---- TAKE CARE OF MISSING DATA SECTION ---- #
 """
@@ -47,6 +18,54 @@ def handle_missing_values():
     pass
 
 
+# ---- TAKE CARE OF MISSING DATA SECTION ---- #
+
+
+# ---- LOAD DATA FROM CSV SECTION ---- #
+def load_data():
+    dotenv.load_dotenv()  # Load the .env file to gather the path of CSV file.
+    dataset = pandas.read_csv(os.getenv('path_data'))  # Gather the dataset from the CSV file.
+
+    # Gather all the values in the columns from the dataset, except the last one; as a numpy array.
+    # Used the ILOC indexer in PANDAS to select the subset of the data and store that in the variable.
+    matrix_features_independent = dataset.iloc[:, :-1].values
+
+    # Gather all the values in the last column from the dataset; as a numpy array.
+    # Used the ILOC indexer in PANDAS to select the subset of the data and store that in the variable.
+    vector_features_dependent = dataset.iloc[:, -1].values
+
+    return matrix_features_independent, vector_features_dependent
+
+
+# ---- LOAD DATA FROM CSV SECTION ---- #
+
+
+# ---- MAIN CODE SECTION ---- #
+
+"""Machine learning model to predict the potential purchase of a car of a person, using 
+the dataset as source input.
+
+Import and processing DataSet.
+"""
+def main():
+    matrix_features_independent, vector_features_dependent = load_data()
+
+    print("Matrix Features Independent:", end='\n')
+    print(matrix_features_independent)
+
+    print("Vector Features Dependent:", end='\n')
+    print(vector_features_dependent)
+# ---- MAIN CODE SECTION ---- #
+
+
+# ---- TAKE CARE OF MISSING DATA SECTION ---- #
+"""
+Function to handle the missing values in the dataset.
+To perform that, in the 'Salary' colum, in case of missing value, the value 
+will be replaced with the average of all the values in the 'Salary' colum.
+"""
+def handle_missing_values():
+    pass
 # ---- TAKE CARE OF MISSING DATA SECTION ---- #
 
 
