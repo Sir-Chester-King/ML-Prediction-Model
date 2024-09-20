989f6defc8b4c877598055f4dbf0c8902f620def
Sir-Chester-King <177522077+Sir-Chester-King@users.noreply.github.com>
2024-09-20 15:18:54 +0200

    Scratch blueprint

---
+++ src/clear_console.py
@@ -0,0 +1,11 @@
+import os
+
+
+def clear():
+    # For Windows environment
+    if os.name == 'nt':
+        os.system('cls')
+
+    # For Linux/Unix environment
+    else:
+        os.system("clear")
