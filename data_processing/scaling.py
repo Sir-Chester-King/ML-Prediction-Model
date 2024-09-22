299a4854b9c2708c79057aed1d15293d4c66f973
Sir-Chester-King <177522077+Sir-Chester-King@users.noreply.github.com>
2024-09-22 16:49:50 +0200

    Updated <Data Processing>

--- data_processing/scaling.py
+++ data_processing/scaling.py
@@ -1,2 +1,10 @@
-def scaling_FEATURE_variables():
-    pass
+# ---- IMPORT SECTION ---- #
+from sklearn.preprocessing import StandardScaler
+
+
+def scaling_FEATURE_variables(feature_train, feature_test):
+    sc = StandardScaler()
+    feature_train[:, 3:] = sc.fit_transform(feature_train[:, 3:])
+    feature_test[:, 3:] = sc.transform(feature_test[:, 3:])
+
+    return feature_train, feature_test
