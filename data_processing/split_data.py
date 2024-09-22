76cf1d4ec59e257b6e7655ec0e510db26b2bfd6c
Sir-Chester-King <177522077+Sir-Chester-King@users.noreply.github.com>
2024-09-22 16:39:52 +0200

    Updated <Data Processing>

--- data_processing/split_data.py
+++ data_processing/split_data.py
@@ -1,6 +1,8 @@
-def split_TRAINING_SET():
-    pass
+# ---- IMPORT SECTION ---- #
+from sklearn.model_selection import train_test_split
 
 
-def split_TEST_SET():
-    pass
+def split_DATASET(feature, target):
+    feature_train, feature_test, target_train, target_test = train_test_split(feature, target, test_size=0.2,
+                                                                              random_state=1)
+    return feature_train, feature_test, target_train, target_test
