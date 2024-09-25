022b35fc2232f953deb4ce04b8615a6450ce5cad
Sir-Chester-King <177522077+Sir-Chester-King@users.noreply.github.com>
2024-09-25 09:22:55 +0200

    Updated <Data Processing>

--- data_processing/split_data.py
+++ data_processing/split_data.py
@@ -1,8 +1,19 @@
+"""
+The Split Dataset is used to split the .csv file into the TRAIN data, used to train the model, and into
+TEST data, used to predict the target data.
+"""
+
 # ---- IMPORT SECTION ---- #
 from sklearn.model_selection import train_test_split
 
 
 def split_DATASET(feature, target):
+    # Split the data into TRAIN and TEST data.
+    """
+    Split:
+    80% Train to train the model;
+    20% Test used to test the model.
+    """
     feature_train, feature_test, target_train, target_test = train_test_split(feature, target, test_size=0.2,
                                                                               random_state=1)
     return feature_train, feature_test, target_train, target_test
