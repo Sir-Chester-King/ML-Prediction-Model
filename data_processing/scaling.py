915d2255f879701dd655668e9731960c1b244545
Sir-Chester-King <177522077+Sir-Chester-King@users.noreply.github.com>
2024-09-25 09:30:40 +0200

    Updated <Data Processing>

--- data_processing/scaling.py
+++ data_processing/scaling.py
@@ -1,10 +1,15 @@
+"""
+Class StandardScaler is used to standardise the feature of dataset.
+"""
+
+
 # ---- IMPORT SECTION ---- #
 from sklearn.preprocessing import StandardScaler
 
 
 def scaling_FEATURE_variables(feature_train, feature_test):
     sc = StandardScaler()
-    feature_train[:, 3:] = sc.fit_transform(feature_train[:, 3:])
-    feature_test[:, 3:] = sc.transform(feature_test[:, 3:])
+    feature_train[:, 3:] = sc.fit_transform(feature_train[:, 3:])  # Train the model with the data scaled
+    feature_test[:, 3:] = sc.transform(feature_test[:, 3:])  # Apply the transformation to the new data
 
     return feature_train, feature_test
