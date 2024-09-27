33fc7f8c3c81f7e17d538b548b00b9f7aa5687bb
Sir-Chester-King <177522077+Sir-Chester-King@users.noreply.github.com>
2024-09-27 09:51:11 +0200

    Updated <Regression>

--- regression/test_multiple_regression/test_multiple_regression.py
+++ regression/test_multiple_regression/test_multiple_regression.py
@@ -4,17 +4,62 @@
 
 
 def main():
-    pass
+    import pandas
+    dataset = pandas.read_csv('50_Startups.csv')
+    feature = dataset.iloc[:, :-1].values
+    target = dataset.iloc[:, -1].values
 
+    # --- ENCODING CATEGORICAL DATA --- #
+    import numpy
+    from sklearn.compose import ColumnTransformer
+    from sklearn.preprocessing import OneHotEncoder
+    column_transform_instance = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [3])],
+                                                  remainder='passthrough')
+    feature = numpy.array(column_transform_instance.fit_transform(feature))
+    feature_numeric = feature[:, 3:]  # gather only the numeric value from feature dataset
 
-def split_data(feature, target):
+    # --- SPLIT DATA --- #
     from sklearn.model_selection import train_test_split
     feature_train, feature_test, target_train, target_test = train_test_split(feature,
-                                                                              target,
-                                                                              test_size=0.2,  # split with 80% train
-                                                                              # and 20% test
-                                                                              random_state=1)
-    return feature_train, feature_test, target_train, target_test
+                                                                              target, test_size=0.2, random_state=1)
+    """print(feature_train)
+    print(target_train)"""
+
+    # --- MULTIPLE LINEAR REGRESSION --- #
+    import matplotlib.pyplot as plt
+    from sklearn.linear_model import LinearRegression
+    mean_feature = numpy.mean(feature)
+    mean_target = numpy.mean(target)
+
+    multiple_lr_model = LinearRegression()  # Create instance of LinearRegression
+
+    '''
+        - Train the model to calculate the straight of linear regression using:
+            1) X1...Xn = feature
+            2) Y = target
+        - .fit() method calculate the best optimal values for the  linear straight:
+            B0 = Intercept
+            B1....Bn = Coefficients
+        - To calculate the optimal parameters of linear straight, the .fit() method use a
+        Ordinary Least Squares (OLS) to calculate them.
+    '''
+    multiple_lr_model.fit(feature_train, target_train)
+    b0 = multiple_lr_model.intercept_
+    bx = multiple_lr_model.coef_
+    print("-" * 40)
+    print("-" * 40)
+    print("Intercept: ", b0)
+    print("Coefficients: ", bx)
+
+    target_test_prediction = multiple_lr_model.predict(feature_test)
+
+    # --- PLOT SCATTER --- #
+    # Creating the graph
+    plt.figure(figsize=(10, 6))  # Defying the size of the graph
+    plot_3d = plt.figure().add_subplot(111, projection='3d')
+
+
+
 
 
 if __name__ == '__main__':
--- regression/test_simple_regression/test_simple_regression.py
+++ regression/test_simple_regression/test_simple_regression.py
@@ -69,30 +69,54 @@ def main():
     print("'Target' PREDICTION Linear Regression Straight", end='\n')
     print(target_test_prediction)
 
-    # Creating the graph
-    plt.figure(figsize=(10, 6))  # Defying the size of the graph
+    def plot_TRAIN():
+        # Creating the graph
+        plt.figure(figsize=(10, 6))  # Defying the size of the graph
 
-    # Creating the scatter plot with real data (feature_train and target_train)
-    plt.scatter(feature_train, target_train, color='blue', label='Real Data')
+        # Creating the scatter plot with real data (feature_train and target_train)
+        plt.scatter(feature_train, target_train, color='blue', label='Real Data')
 
-    # Creating the linear regression straight
-    plt.plot(feature_train, target_test_prediction, color='red', label='Regression Straight')
+        # Creating the linear regression straight
+        plt.plot(feature_train, target_test_prediction, color='red', label='Regression Straight')
 
-    # Defying label and title of plot
-    plt.title('Scatter Plot - Dataset TRAINING', fontsize=14)
-    plt.xlabel('Year of experience', fontsize=12)
-    plt.ylabel('Salary', fontsize=12)
+        # Defying label and title of plot
+        plt.title('Scatter Plot - Dataset TRAINING', fontsize=14)
+        plt.xlabel('Year of experience', fontsize=12)
+        plt.ylabel('Salary', fontsize=12)
 
-    # View the mean (average) of FEATURE and TARGET
-    plt.axhline(y=mean_target, color='green', linestyle='--', label=f'Average Salary = {mean_target}')
-    plt.axvline(x=mean_feature, color='orange', linestyle='--', label=f'Average Year of experience = {mean_feature}')
+        # View the mean (average) of FEATURE and TARGET
+        plt.axhline(y=mean_target, color='green', linestyle='--', label=f'Average Salary = {mean_target}')
+        plt.axvline(x=mean_feature, color='orange', linestyle='--',
+                    label=f'Average Year of experience = {mean_feature}')
 
-    # Show Legend
-    plt.legend()
+        # Show Legend
+        plt.legend()
 
-    # Show plot
-    plt.grid(True)
-    plt.show()
+        # Show plot
+        plt.grid(True)
+        plt.show()
+
+    def plot_TEST():
+        # Creating the scatter plot with real data (feature_train and target_train)
+        plt.scatter(feature_test, target_test, color='blue', label='Real Data')
+
+        # Creating the linear regression straight
+        plt.plot(feature_train, target_test_prediction, color='red', label='Regression Straight')
+
+        # Defying label and title of plot
+        plt.title('Scatter Plot - Dataset TARGET', fontsize=14)
+        plt.xlabel('Year of experience', fontsize=12)
+        plt.ylabel('Salary', fontsize=12)
+
+        # Show Legend
+        plt.legend()
+
+        # Show plot
+        plt.grid(True)
+        plt.show()
+
+    plot_TRAIN()
+    plot_TEST()
 
 
 def split_data(feature, target):
