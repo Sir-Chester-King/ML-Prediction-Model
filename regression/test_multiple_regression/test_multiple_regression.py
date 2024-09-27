2d6cdfc71f75ad9162cc12b86eadc7a519b811bb
Sir-Chester-King <177522077+Sir-Chester-King@users.noreply.github.com>
2024-09-27 10:35:34 +0200

    Updated <Regression>

--- regression/test_multiple_regression/test_multiple_regression.py
+++ regression/test_multiple_regression/test_multiple_regression.py
@@ -16,17 +16,13 @@ def main():
     column_transform_instance = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [3])],
                                                   remainder='passthrough')
     feature = numpy.array(column_transform_instance.fit_transform(feature))
-    feature_numeric = feature[:, 3:]  # gather only the numeric value from feature dataset
 
     # --- SPLIT DATA --- #
     from sklearn.model_selection import train_test_split
     feature_train, feature_test, target_train, target_test = train_test_split(feature,
                                                                               target, test_size=0.2, random_state=1)
-    """print(feature_train)
-    print(target_train)"""
 
     # --- MULTIPLE LINEAR REGRESSION --- #
-    import matplotlib.pyplot as plt
     from sklearn.linear_model import LinearRegression
     mean_feature = numpy.mean(feature)
     mean_target = numpy.mean(target)
@@ -52,14 +48,31 @@ def main():
     print("Coefficients: ", bx)
 
     target_test_prediction = multiple_lr_model.predict(feature_test)
+    print("-" * 40)
+    print("-" * 40)
+    print("TARGET TEST")
+    print(target_test)
 
-    # --- PLOT SCATTER --- #
-    # Creating the graph
-    plt.figure(figsize=(10, 6))  # Defying the size of the graph
-    plot_3d = plt.figure().add_subplot(111, projection='3d')
+    print("-" * 40)
+    print("-" * 40)
+    print("PREDICTION TEST")
+    print(target_test_prediction)
 
+    numpy.set_printoptions(precision=2)
 
+    print("-" * 40)
+    print("-" * 40)
+    print("CONCAT VECTORS - TARGET TEST & PREDICTION TEST")
 
+    # Here the concat of 2 Numpy array:
+    # 1: The array of the TARGET TEST
+    # 2: The array of PREDICTION TEST
+    # Using the Reshape function to a better view of the print, from horizontal to vertical
+    vector_concat = numpy.concatenate(
+        (target_test.reshape(len(target_test), 1),
+         target_test_prediction.reshape(len(target_test_prediction), 1)
+         ), 1)
+    print(vector_concat)
 
 
 if __name__ == '__main__':
